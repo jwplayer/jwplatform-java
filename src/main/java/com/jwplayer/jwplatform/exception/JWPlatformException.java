3de1fd26ea1a7e4ceba4c352cb1c0732a50ba951
Steve Whelan <swhelan091@gmail.com>
2019-12-05 02:19:33 -0500

    Use static map over switch statement

--- src/main/java/com/jwplayer/jwplatform/exception/JWPlatformException.java
+++ src/main/java/com/jwplayer/jwplatform/exception/JWPlatformException.java
@@ -3,12 +3,15 @@ package com.jwplayer.jwplatform.exception;
 /** Base class for JWPlatform Exceptions. */
 public class JWPlatformException extends Exception {
 
-  /**
-   * Base exception class.
-   *
-   * @param message - exception message
-   */
-  public JWPlatformException(final String message) {
-    super(message);
-  }
+    public JWPlatformException() {
+    }
+
+    /**
+     * Instance of an UnknownException.
+     *
+     * @param message the exception message
+     */
+    public JWPlatformException(final String message) {
+        super(message);
+    }
 }
